eb2eadd04bf8fcda1bc5dec60cbdaf0173175f9e
cometj03 <harryjung1001@gmail.com>
2024-02-05 15:05:55 +0900

    chore: java 버전 11

--- yls/build.gradle.kts
+++ yls/build.gradle.kts
@@ -19,16 +19,16 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
