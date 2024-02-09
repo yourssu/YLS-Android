c8feeac4d327f3eb5889a3d81355d27bab80b8d9
cometj03 <harryjung1001@gmail.com>
2024-02-09 21:04:54 +0900

    chore: 클래스 구조 구축

--- yls/src/main/kotlin/com/yourssu/logging/system/YLS.kt
+++ yls/src/main/kotlin/com/yourssu/logging/system/YLS.kt
@@ -1,3 +1,41 @@
 package com.yourssu.logging.system
 
-class YLS
+import okhttp3.MediaType
+import okhttp3.Request
+import okhttp3.RequestBody
+
+abstract class Logger {
+    abstract fun log(event: YLSEventData)
+}
+
+class YLS {
+    open class RemoteLogger(
+        val url: String,
+        val platform: String,       // ex: android
+        val serviceName: String,    // ex: Soomsil
+    ) : Logger() {
+
+
+        override fun log(event: YLSEventData) {
+
+        }
+
+        private fun createRequest(json: String): Request =
+            Request.Builder()
+                .url(url)
+                .put(RequestBody.create(MediaType.parse("application/json"), json))
+                .build()
+    }
+
+    companion object : Logger() {
+        private lateinit var logger: Logger
+
+        fun init(logger: Logger) {
+            this.logger = logger
+        }
+
+        override fun log(event: YLSEventData) {
+            this.logger.log(event)
+        }
+    }
+}
