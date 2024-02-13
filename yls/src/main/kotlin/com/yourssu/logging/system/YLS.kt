72a899b42d807c0082070c4179c7313ca97bf471
cometj03 <harryjung1001@gmail.com>
2024-02-13 11:47:44 +0900

    #4 feat: queueing 기능 추가

--- yls/src/main/kotlin/com/yourssu/logging/system/YLS.kt
+++ yls/src/main/kotlin/com/yourssu/logging/system/YLS.kt
@@ -7,24 +7,34 @@ import okhttp3.RequestBody
 
 class YLS {
     abstract class Logger {
-        protected abstract fun log(user: String, timestamp: String, event: Map<String, Any>)
+        protected abstract fun log(eventData: YLSEventData)
     }
 
     open class DefaultLogger(
         val url: String,
         val platform: String,       // ex: android
         val serviceName: String,    // ex: Soomsil
+        val queueCapacity: Int = 10,
     ) : Logger() {
         private val client = OkHttpClient()
+        private val cache = ArrayList<YLSEventData>(queueCapacity)
 
-        override fun log(user: String, timestamp: String, event: Map<String, Any>) {
-            val eventData = YLSEventData(user, timestamp, event)
-            val json = eventData.toJsonString()
-            val request = Request.Builder()
-                .url(url)
-                .put(RequestBody.create(MediaType.parse("application/json"), json))
-                .build()
-            client.newCall(request).execute()
+        override fun log(eventData: YLSEventData) {
+            if (cache.isNotEmpty() && cache.last() == eventData) {
+                return
+            }
+            cache.add(eventData)
+
+            // todo 디테일하게 할 필요가 있다
+            if (cache.size >= queueCapacity) {
+                val json = "[" + cache.joinToString(", ") { it.toJsonString() } + "]"
+                val request = Request.Builder()
+                    .url(url)
+                    .put(RequestBody.create(MediaType.parse("application/json"), json))
+                    .build()
+                client.newCall(request).execute()
+                cache.clear()
+            }
         }
     }
 
