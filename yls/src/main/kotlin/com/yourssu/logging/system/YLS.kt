4189b91456732b23159f0f33ea9ce608af5c8c67
정해성 <harryjung1001@gmail.com>
2024-02-15 23:35:44 +0900

    feat: 기본 함수 구현

--- yls/src/main/kotlin/com/yourssu/logging/system/YLS.kt
+++ yls/src/main/kotlin/com/yourssu/logging/system/YLS.kt
@@ -1,11 +1,22 @@
 package com.yourssu.logging.system
 
-class YLS {
-    abstract class Logger
+class YLS private constructor() {
+    abstract class Logger {
+        abstract fun log(url: String, eventData: YLSEventData)
+    }
 
-    open class DebugLogger : Logger()
+    open class DebugLogger : Logger() {
+        override fun log(url: String, eventData: YLSEventData) {
+
+        }
+    }
 
     companion object {
+        private lateinit var logger: Logger
+        private lateinit var defaultMap: Map<String, Any>
+        private lateinit var url: String
+        private lateinit var userID: String
+
         fun init(
             platform: String,
             serviceName: String,
@@ -13,13 +24,25 @@ class YLS {
             url: String,
             logger: Logger,
         ) {
-
+            this.defaultMap = mapOf("platform" to platform, "serviceName" to serviceName)
+            this.userID = user
+            this.url = url
+            this.logger = logger
         }
 
         fun randomId(): String = "aaa"
 
         fun log(vararg events: Pair<String, Any>) {
+            if (!::logger.isInitialized) throw AssertionError()
 
+            val eventData = YLSEventData(
+                hashedID = hashing(userID),
+                timestamp = "",
+                event = events.toMap() + defaultMap,
+            )
+            logger.log(url, eventData)
         }
+
+        private fun hashing(id: String) = "asdf"
     }
 }
