6d41d8610d0b6a7c4d101c2923759af7d5de9ab6
cometj03 <harryjung1001@gmail.com>
2024-02-17 15:51:52 +0900

    feat: putLog(YLSEventData)

--- yls/src/main/kotlin/com/yourssu/logging/system/worker/RemoteLoggingWorker.kt
+++ yls/src/main/kotlin/com/yourssu/logging/system/worker/RemoteLoggingWorker.kt
@@ -3,10 +3,13 @@ package com.yourssu.logging.system.worker
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.google.gson.Gson
 import com.yourssu.logging.system.HttpService
+import com.yourssu.logging.system.YLSEventData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 internal class RemoteLoggingWorker(
     appContext: Context,
@@ -17,6 +20,7 @@ internal class RemoteLoggingWorker(
         inputData.getString(KEY_LOGGING_URL)?.let {
             Retrofit.Builder()
                 .baseUrl(it)
+                .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(HttpService::class.java)
         }
@@ -24,7 +28,8 @@ internal class RemoteLoggingWorker(
 
     override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
         inputData.getString(KEY_LOGGING_DATA)?.let { json ->
-            val response = service?.putLog(json) ?: return@let Result.failure()
+            val eventData = Gson().fromJson(json, YLSEventData::class.java)
+            val response = service?.putLog(eventData) ?: return@let Result.failure()
 
             // 코드에 따른 상세한 처리
             if (response.code() in 200..299) {
