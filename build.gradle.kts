e4909c9e11953773dc8eb25a527486395612a230
cometj03 <harryjung1001@gmail.com>
2024-02-18 16:32:15 +0900

    #3 java 17, maven gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,9 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.android.library") version "8.2.1" apply false
 }
+
+buildscript {
+    dependencies {
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
+    }
+}
