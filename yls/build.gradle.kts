c3d4537dabd41cb9cd657a5694c344f43b030ad6
cometj03 <harryjung1001@gmail.com>
2024-02-18 16:38:41 +0900

    #3

--- yls/build.gradle.kts
+++ yls/build.gradle.kts
@@ -33,6 +33,16 @@ android {
     }
 }
 
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+            }
+        }
+    }
+}
+
 dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
