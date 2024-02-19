31a3033108026ec75c68e078d50a7ee0a4aba05c
cometj03 <harryjung1001@gmail.com>
2024-02-19 22:57:11 +0900

    docs: 주석 추가

--- yls/src/main/kotlin/com/yourssu/logging/system/YLS.kt
+++ yls/src/main/kotlin/com/yourssu/logging/system/YLS.kt
@@ -16,6 +16,7 @@ import java.util.Date
  * Yourssu Logging System, inspired by [Timber](https://github.com/JakeWharton/timber)
  *
  * ```kotlin
+ * // 사용 예
  * // Application의 onCreate()에서 초기화
  * YLS.init(
  *      platform = "android",
@@ -59,9 +60,12 @@ class YLS private constructor() {
     }
 
     /**
-     * 설정된 url로 로그 데이터를
+     * 로그 데이터를 `url`로 PUT 요청을 보내는 Logger입니다.
      *
-     * [RemoteLoggingWorker]를 사용하여 백그라운드에서 로그 이벤트를 처리합니다.
+     * [RemoteLoggingWorker]를 사용하여 백그라운드에서 http 요청을 처리합니다.
+     *
+     * @param url 로깅 요청을 보낼 url
+     * @param context WorkManager.getInstance()에 필요한 [Context]
      */
     open class RemoteLogger(private val url: String, context: Context) : Logger() {
 
@@ -80,6 +84,7 @@ class YLS private constructor() {
         }
     }
 
+    /** 디버깅 할 때 사용할 수 있는 Logger입니다. */
     open class DebugLogger : Logger() {
         override fun log(eventData: List<YLSEventData>) {
             Log.d("YLS.DebugLogger", Gson().toJson(eventData))
@@ -91,6 +96,13 @@ class YLS private constructor() {
         private lateinit var defaultEvent: Map<String, Any>
         private lateinit var userID: String
 
+        /**
+         * 최초 YLS 초기화. 앱의 Application.onCreate()에서 초기화하는 것을 권장합니다.
+         *
+         * @param platform 플랫폼 이름
+         * @param user 유저 ID. 비로그인 상태라면 [YLS.generateRandomId]로 임시 ID 생성 권장
+         * @param logger 실질적인 로깅 객체. [YLS.Logger]의 서브 타입
+         */
         fun init(
             platform: String,
             user: String,
@@ -122,6 +134,7 @@ class YLS private constructor() {
             logger.enqueue(eventData)
         }
 
+        /** Logger에 남아있는 로그 데이터를 모두 내보낸 후 큐를 비웁니다. */
         fun flush() {
             if (!::logger.isInitialized) {
                 throw AssertionError("Not initialized! : YLS.init()을 먼저 호출해 주세요.")
@@ -129,11 +142,21 @@ class YLS private constructor() {
             logger.flush()
         }
 
+        /**
+         * ASCII 33 ~ 126 사이의 문자로 `length`만큼의 길이의 랜덤 문자열을 반환합니다.
+         *
+         * @return random string with length `length`
+         */
         fun generateRandomId(length: Int): String {
             val charset = '!'..'~' // ASCII 33 ~ 126
             return (1..length).map { charset.random() }.joinToString("")
         }
 
+        /**
+         * 현재 시각을 ISO 8601 포맷의 문자열을 반환합니다.
+         *
+         * @return ISO 8601 format string of current
+         */
         @SuppressLint("SimpleDateFormat")
         fun getTimestampISO8601(): String =
             SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(Date())
